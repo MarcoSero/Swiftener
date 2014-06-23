57741fe0bdafebe6cf0b550ea54db7446bdb3e3b
Marco Sero <marco@marcosero.com>
2014-06-23 21:53:11 +0100

    Initial Commit

---
+++ Swiftener/Swiftener.h
@@ -0,0 +1,19 @@
+//
+//  Swiftener.h
+//  Swiftener
+//
+//  Created by Marco Sero on 23/06/2014.
+//  Copyright (c) 2014 Marco Sero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swiftener.
+FOUNDATION_EXPORT double SwiftenerVersionNumber;
+
+//! Project version string for Swiftener.
+FOUNDATION_EXPORT const unsigned char SwiftenerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swiftener/PublicHeader.h>
+
+
