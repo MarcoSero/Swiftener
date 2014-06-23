aded2731905edd1467ecea038362db7cbb2059a8
Marco Sero <marco@marcosero.com>
2014-06-23 21:57:43 +0100

    first commit

---
+++ Swiftener/Operators/PatternMatching.swift
@@ -0,0 +1,11 @@
+//
+//  PatternMatching.swift
+//  Swiftener
+//
+//  Created by Marco Sero on 23/06/2014.
+//  Copyright (c) 2014 Marco Sero. All rights reserved.
+//
+
+func ~= <T: Comparable>(left: T[], right: T[]) -> Bool {
+  return left == right
+}
